623c68ad98937961396d45f14529fc6b7912acb9
shonmathew19 <shonformal@gmail.com>
2024-10-11 14:26:15 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     return (
         <>
             <Header />
-
+            <Link to={'/home'} className='mb-3 mt-3'><button className=' login-second-col-btn text-light rounded-5'> Back to <i class="fa-solid fa-house ms-2"></i></button></Link>
             <Footer />
         </>
     )
